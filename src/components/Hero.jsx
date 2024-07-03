8657802a115295b0a42f7392da6213418ddd95f4
maryamnawas <maryamnawas2002@gmail.com>
2024-07-03 13:28:25 +0530

    made some text to bold

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,8 +6,8 @@ const Hero = () => {
       <div className="container mx-auto flex flex-col lg:flex-row items-center justify-center">
         {/* Left Column */}
         <div className="lg:w-[50%] px-4 lg:px-13">
-          <h1 className="text-2xl lg:text-xl text-[#FF725E] mb-4 lg:mb-4">Welcome to Move Mate!</h1>
-          <h2 className="text-lg lg:text-4xl text-black font-bold mb-3 lg:mb-5">Embark on Your Personalized Rehabilitation Journey</h2>
+        <h1 className="text-2xl lg:text-xl text-[#FF725E] mb-4 lg:mb-4 font-bold">Welcome to moveMate!</h1>
+        <h2 className="text-lg lg:text-4xl text-black font-bold mb-3 lg:mb-5">Embark on Your Personalized Rehabilitation Journey</h2>
           <p>Experience the power of personalized recovery at your fingertips. Where your journey to rehabilitation begins with tailored exercises and support designed just for you.</p>
         </div>
         {/* Right Column */}
