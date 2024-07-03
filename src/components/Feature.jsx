918ff2681109db64d4817a93e96052d08183de0e
maryamnawas <maryamnawas2002@gmail.com>
2024-07-03 14:52:11 +0530

    made a few changes

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -37,8 +37,8 @@ export default function Feature() {
     <div className="bg-white py-10 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
-          <h2 className="text-base font-semibold leading-7 text-indigo-600">Fetures</h2>
-          <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+        <h2 className="text-2xl font-semibold leading-7 text-indigo-600">Features</h2>
+        <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Everything you need to deploy your app
           </p>
           <p className="mt-6 text-lg leading-8 text-gray-600">
