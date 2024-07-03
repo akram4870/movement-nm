32841eb1d100b15e6a90afe2ab11c6f8251e8508
maryamnawas <maryamnawas2002@gmail.com>
2024-07-03 13:42:19 +0530

    increased the size of text in hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
       <div className="container mx-auto flex flex-col lg:flex-row items-center justify-center">
         {/* Left Column */}
         <div className="lg:w-[50%] px-4 lg:px-13">
-        <h1 className="text-2xl lg:text-xl text-[#FF725E] mb-4 lg:mb-4 font-bold">Welcome to moveMate!</h1>
+        <h1 className="text-4xl lg:text-3xl text-[#FF725E] mb-4 lg:mb-4 font-bold">Welcome to moveMate!</h1>
         <h2 className="text-lg lg:text-4xl text-black font-bold mb-3 lg:mb-5">Empowering Your Recovery Journey</h2>
           <p>Experience the power of personalized recovery with moveMate. Our app uses advanced hand-tracking technology to provide engaging games and tailored exercises, helping you regain mobility and independence. Transform your rehabilitation into an enjoyable and motivating journey with moveMate.
           </p>
